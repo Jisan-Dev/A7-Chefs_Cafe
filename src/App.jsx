c399654cf6eeb8a08d697211999f63eb86c4b29b
Jisun696 <istiakkashem35@gmail.com>
2024-03-15 22:28:56 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-200 font-lexend text-4xl">React</h1>
+      <h1 className="bg-green-200 font-fira text-2xl">React</h1>
+    </>
+  );
+}
+
+export default App;
