b16847e78cace4925d454bf4bb9fea3be967d05c
Jisun696 <istiakkashem35@gmail.com>
2024-03-16 00:51:04 +0600

    added header part using daisyUI and react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Header from './header';
+
 function App() {
   return (
     <>
-      <h1 className="bg-green-200 font-lexend text-4xl">React</h1>
-      <h1 className="bg-green-200 font-fira text-2xl">React</h1>
+      <Header />
     </>
   );
 }
---
+++ src/header/index.jsx
@@ -0,0 +1,79 @@
+import { HiOutlineUserCircle } from 'react-icons/hi2';
+
+const Header = () => {
+  return (
+    <header className="font-lexend container mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start max-sm:w-full">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Recipes</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Search</a>
+              </li>
+              <label className="input input-bordered flex items-center gap-2 rounded-full mr-4">
+                <input type="text" className="w-full" placeholder="Search" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5 opacity-70">
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </label>
+            </ul>
+          </div>
+          <a href="#" className="text-2xl md:text-3xl text-slate-900 font-bold">
+            Recipe Calories
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-base text-slate-900 text-opacity-70">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Recipes</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Search</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <label className="input input-bordered  hidden md:flex items-center gap-2 rounded-full mr-4">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5 opacity-70">
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+          <div className="bg-emerald-500 rounded-[50px] p-2 max-sm:mr-2">
+            <HiOutlineUserCircle className="text-3xl text-slate-900 font-black" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
